8ed6ab589f95f5692042d0f5542a46615a446374
fizaan-ali <fizaan.mughal.01@gmail.com>
2025-10-22 18:36:06 -0700

    Formatted the file

--- x.cpp
+++ x.cpp
@@ -2,79 +2,92 @@
 #include <string>
 #include <cctype>
 using namespace std;
-class Employee {
-    private:
-        string name, department, position;
-        int id;
-    public:
-    Employee(){
+class Employee
+{
+private:
+    string name, department, position;
+    int id;
+
+public:
+    Employee()
+    {
         name = department = position = "";
         id = 0;
     }
-    Employee(int id_, string name_){
+    Employee(int id_, string name_)
+    {
         id = id_;
         name = name_;
         department = position = "";
     }
-    Employee(string name_, int id_, string department_, string position_){
+    Employee(string name_, int id_, string department_, string position_)
+    {
         name = name_;
         id = id_;
         department = department_;
         position = position_;
         cout << endl;
     }
-        void getInfo(){
-            cout << "Enter Employee's name: ";
-            cin >> name;
-            cout << "Enter Employee's id: ";
-            cin >> id;
-            cout << "Enter Employee's department: ";
-            cin >> department;
-            cout << "Enter Employee's position: ";
-            cin >> position;
-            cout << endl;
+    void getInfo()
+    {
+        cout << "Enter Employee's name: ";
+        cin >> name;
+        cout << "Enter Employee's id: ";
+        cin >> id;
+        cout << "Enter Employee's department: ";
+        cin >> department;
+        cout << "Enter Employee's position: ";
+        cin >> position;
+        cout << endl;
+    }
+    void setInfo(string _name, int _id, string _department, string _position)
+    {
+        name = _name;
+        id = _id;
+        department = _department;
+        position = _position;
+        cout << endl;
+    }
+    void printInfo()
+    {
+        cout << "Employee Details: " << endl;
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+        cout << "Department: " << department << endl;
+        cout << "Position: " << position << endl;
+    }
+    void convertCaps()
+    {
+        for (int i = 0; i < name.length(); i++)
+        {
+            if (islower(name[i]))
+                name[i] = toupper(name[i]);
+            else
+                name[i] = tolower(name[i]);
         }
-        void setInfo(string _name, int _id, string _department, string _position){
-            name = _name;
-            id = _id;
-            department = _department;
-            position = _position;
-            cout << endl;
+        for (int i = 0; i < department.length(); i++)
+        {
+            if (islower(department[i]))
+                department[i] = toupper(department[i]);
+            else
+                department[i] = tolower(department[i]);
         }
-        void printInfo(){
-            cout << "Employee Details: " << endl;
-            cout << "Name: " << name << endl;
-            cout << "ID: " << id << endl;
-            cout << "Department: " << department << endl;
-            cout << "Position: " << position << endl;
+        for (int i = 0; i < position.length(); i++)
+        {
+            if (islower(position[i]))
+                position[i] = toupper(position[i]);
+            else
+                position[i] = tolower(position[i]);
         }
-        void convertCaps(){
-            for(int i=0; i<name.length(); i++){
-                if(islower(name[i]))
-                    name[i] = toupper(name[i]);
-                else 
-                    name[i] = tolower(name[i]);
-            }
-            for(int i=0; i<department.length(); i++){
-                if(islower(department[i]))
-                    department[i] = toupper(department[i]);
-                else 
-                    department[i] = tolower(department[i]);
-            }
-            for(int i=0; i<position.length(); i++){
-                if(islower(position[i]))
-                    position[i] = toupper(position[i]);
-                else 
-                    position[i] = tolower(position[i]);
-            }
-        }
-
-    ~Employee(){
-        cout << "Destructor executed...." << endl;
     }
 
+    ~Employee()
+    {
+        cout << "Destructor executed...." << endl;
+    }
 };
-int main() {
+int main()
+{
     Employee e1, e2;
     e1.getInfo();
     e2.setInfo("Fizaan", 55, "IT", "Developer");
@@ -86,4 +99,4 @@ int main() {
     e1.printInfo();
     e2.printInfo();
     return 0;
-} 
+}
